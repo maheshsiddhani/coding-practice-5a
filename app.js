88dc0c97e9e1e5538cf3ab09b8a652f80b69d272
mahesh <maheshsiddhani@gmail.com>
2023-12-25 02:57:04 +0000

    post movie

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+//Get movies
+app.get("/movies/", async (request, response) => {
+  const getMoviesQuery = `
+    SELECT 
+      *
+    FROM
+    movie;`;
+  const movies = await db.all(getMoviesQuery);
+  response.send(
+    movies.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
+  );
+});
+
+//Post Movie
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const addMovieQuery = `
+    INSERT INTO movie(director_id,move_name,lead_actor)
+    VALUES(
+        ${directorId},
+        '${movieName}',
+        '${leadActor}';
+        )`;
+  const dbResponse = await db.run(addMovieQuery);
+  response.send("Movie Successfully Added");
+});
